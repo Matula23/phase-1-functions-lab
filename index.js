54710e46bf446c959f48d69e61adeec822cd11f7
Maciej Matula <maciek.matula@gmail.com>
2022-05-17 21:44:07 -0500

    commit tree ce021e6b8c2f7bb23a7c22927454e87922b04d94

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const HQ = 42;
+const distanceFromHqInBlocks = function(location){
+    const distance = Math.abs(HQ - location);
+    return distance
+}
+
+const distanceFromHqInFeet = function(location){
+    return distanceFromHqInBlocks(location) * 264
+}
+
+const distanceTravelledInFeet = function(loc1,loc2){
+    const distance = Math.abs(loc1 - loc2) * 264;
+    return distance
+}
+
+function calculatesFarePrice(loc1, loc2){
+    const distance = distanceTravelledInFeet(loc1, loc2);
+
+    if(distance <= 400){
+        return 0
+    } else if (distance < 2000){
+        return (distance - 400) * .02;
+    } else if (distance < 2500){
+        return 25;
+    }else{
+        return "cannot travel that far"
+    }
+}
